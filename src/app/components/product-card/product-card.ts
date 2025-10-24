0d2bcd0008574183f446a8dbac93d841137c416e
vanessamarely <vanessamarely@gmail.com>
2025-10-24 16:00:10 -0500

    feat: step 5 product card and product list updated

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ProductInterface } from '../../models/product.model';
+import { CommonModule } from '@angular/common'; // Para el pipe 'currency'
+import { RouterLink } from '@angular/router'; // Para el enlace al detalle
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
 export class ProductCard {
-
+  product = input.required<ProductInterface>();
 }
--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Product as ProductService } from '../../services/product';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.css',
 })
 export class ProductList {
+  // Inyectamos el servicio de productos
+  // inject reemplaza la necesidad de un constructor para la inyección de dependencias
+  private productService = inject(ProductService);
 
+  // Usamos toSignal para convertir el Observable<Product[]> en un Signal<Product[] | undefined>
+  // Angular gestiona la suscripción y desuscripción automáticamente.
+  public products = toSignal(this.productService.getProducts());
+
+  //en el constructor se puede definir la inyección de dependencias de servicios
+  constructor() {
+    // Opcional: Puedes ver el valor inicial (undefined) y luego el array
+    console.log('Valor inicial del signal products:', this.products());
+  }
 }
