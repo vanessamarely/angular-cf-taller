95db41a95202df17dedfe64b5edf8c6203831a15
vanessamarely <vanessamarely@gmail.com>
2025-10-24 15:35:15 -0500

    feat: step 3 routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { ProductList } from './pages/product-list/product-list';
+import { ProductDetail } from './pages/product-detail/product-detail';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Inicio - Mini Tienda' },
+  { path: 'products', component: ProductList, title: 'Productos - Mini Tienda' },
+  { path: 'product/:id', component: ProductDetail, title: 'Detalle Producto' },
+  { path: '**', component: NotFound, title: '404 - No Encontrado' },
+];
